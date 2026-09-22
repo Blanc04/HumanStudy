0912f7cfdef08081a45dde8baa767738bbebb403
DESKTOP-5QGIGNM\admin <d16udonne04@gmail.com>
2026-09-22 17:25:06 +0900

    09/22

---
+++ workspace_python/07_elastic/api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from router.es_router import router as es_router
+from router.embedding_router import router as embedding_router
+
+app = FastAPI()
+app.include_router(es_router)
+
+@app.get('/')
+def home():
+    return 'welcome!'
+
+if __name__=="__main__":
+    import uvicorn
+    uvicorn.run('api:app', port=8000, reload=True, host="0.0.0.0")
